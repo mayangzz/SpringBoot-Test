16bbf6e0edfe19569662a8391f6d9fb2070891b9
mayangzz <1033515525@qq.com>
2019-10-18 22:19:38 +0800

    修改Bug

--- src/main/java/cn/my/application/pojo/User.java
+++ src/main/java/cn/my/application/pojo/User.java
@@ -1,4 +1,13 @@
 package cn.my.application.pojo;
 
 public class User {
+
+    private int userid;
+
+    public void setUserid(int userid){
+        this.userid = userid;
+    }
+    public int getUserid(){
+        return this.userid;
+    }
 }
